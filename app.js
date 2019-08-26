e05e356c8a20babb424bbf5090b348d8c5b6e69f
guptarenu240489 <guptarenu24@gmail.com>
2019-08-26 12:30:15 +0530

    üsing express

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    console.log('From add-product route');
+    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>');
+});
+
+/* // Rigt now this middleware will trigger for both get and post request
+app.use('/product', (res, req, next) => {
+    console.log(res.body);
+    req.redirect('/');
+});
+*/
+
+// Now this will trigger only for post request
+app.post('/product', (res, req, next) => {
+    console.log(res.body);
+    req.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
     console.log('Ín middleware');
-    next();
+    res.send('<h1>Hello from express!!!</h1>')
 });
 
 app.use((req, res, next) => {
