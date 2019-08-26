04ef0e1de8d904c8e3c683a58d5fba22b66c3af7
guptarenu240489 <guptarenu24@gmail.com>
2019-08-26 13:32:46 +0530

    adding router to express

--- app.js
+++ app.js
@@ -1,36 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const adminRoutes  = require('./routes/admin');
+const shopRoutes= require('./routes/shop');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+/* If we want any url to have common route prepended in start e.g. /admin/add-product, /admin/product
+we need to add it to filter
+app.use('/admin', adminRoutes);
 
-app.use('/add-product', (req, res, next) => {
-    console.log('From add-product route');
-    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>');
-});
+Now all routes in adminRoute will have baseUrl /admin added to them */
 
-/* // Rigt now this middleware will trigger for both get and post request
-app.use('/product', (res, req, next) => {
-    console.log(res.body);
-    req.redirect('/');
-});
-*/
-
-// Now this will trigger only for post request
-app.post('/product', (res, req, next) => {
-    console.log(res.body);
-    req.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    console.log('Ín middleware');
-    res.send('<h1>Hello from express!!!</h1>')
-});
-
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 app.use((req, res, next) => {
-    console.log('In next middleware');
-    res.send('<h1>Hello from express</h1>');
-});
-
+    res.status(404).send('<h1>Page Not Found </h1>');
+})
 app.listen(3000);
