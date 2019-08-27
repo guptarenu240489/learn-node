3190839194e1984c9e2839ea1a1fcfcdf6f90292
guptarenu240489 <guptarenu24@gmail.com>
2019-08-27 14:46:44 +0530

    using templating engine pug

--- routes/admin.js
+++ routes/admin.js
@@ -6,17 +6,21 @@ const router = express.Router();
 
 const rootDir = require('../utils/path');
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
     // one way
     // res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 
     //another and clean way
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
 });
 
-router.post('/product', (res, req, next) => {
-    console.log(res.body);
+router.post('/add-product', (res, req, next) => {
+    products.push({title: res.body.title});
     req.redirect('/');
 });
 
-module.exports = router;
+exports.router = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,14 @@ const router = express.Router();
 
 const rootDir = require('../utils/path');
 
+const adminData = require('../routes/admin');
+
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    console.log(adminData.products);
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    // We already mention view engine in app.js os it will look for shop.pug
+    // inside views folder as views was also mentions using app.set
+    res.render('shop', {prods: adminData.products, pageTitle: 'Shops', path: '/'});
 });
 
 module.exports = router;
