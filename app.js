6220172dac0c8c442837131b3cb5efb1584a7102
guptarenu240489 <guptarenu24@gmail.com>
2019-08-28 12:12:20 +0530

    adding ejs tempalting engine

--- app.js
+++ app.js
@@ -2,20 +2,14 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const adminData  = require('./routes/admin');
 const shopRoutes= require('./routes/shop');
 
 const app = express();
 
-app.engine('hbs', expressHbs({
-    layoutsDir: 'views/layouts',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-}));
 // To tell node to use this templating engine
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 // To tell node where to look for templates
 app.set('views', 'views');
 app.use(bodyParser.urlencoded({extended: false}));
